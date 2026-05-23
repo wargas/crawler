969baa26639d02132251bdbea3d3c281f4f87c25
Wargas Teixeira <teixeira.wargas@gmail.com>
2026-05-23 06:20:30 -0300

    html-parse

---
+++ index.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'bun:test'
+import { Crawler } from "./index";
+
+test(`get page title`, async function() {
+        
+    const c = Crawler.factory();
+    
+    await c.client.get("https://google.com")
+
+    expect(c.document.title).toBe(`Google`)
+    
+})
--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@ import type { Got } from "got";
 import got from "got";
 import { CookieJar } from "tough-cookie";
 import FileCookieStore from "tough-cookie-file-store";
+import { parseHTML } from "linkedom"
 
 export class Crawler {
     client!: Got
     cookieJar!: CookieJar
+    html = ``
+    document = parseHTML(``).document
 
     static factory() {
         const instance = new Crawler()
@@ -13,7 +16,21 @@ export class Crawler {
         instance.cookieJar = new CookieJar(new FileCookieStore(`cookies.json`))
 
         instance.client = got.extend({
-            cookieJar: instance.cookieJar
+            hooks: {
+                afterResponse: [
+                    res => {
+                        if(typeof res.body == `string`) {
+                            instance.html =  res.body
+                            instance.document = parseHTML(res.body).document
+                        }
+                        
+                        return res
+                    }
+                ]
+            }
+            // followRedirect:false,
+            // cookieJar: instance.cookieJar,
+            
         })
 
         return instance;
